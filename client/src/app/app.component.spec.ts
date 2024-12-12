7cdebfab28e9b9d0af87b7806bc0de05289cbdcc
Hamza Latif <128707594+HamzaLatifGIT@users.noreply.github.com>
2024-12-12 23:20:18 +0500

    Update app.component.spec.ts

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent], // Fixed incorrect usage of 'imports'
     }).compileComponents();
   });
 
